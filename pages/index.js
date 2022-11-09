87dc2f00d1e79b00342bb5cf6884731c38bd10a2
danielrio <danielrio126@gmail.com>
2022-11-09 21:59:41 +0630

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Fragment } from 'react'
+import { Navbar } from 'flowbite-react'
 
 export default function Home() {
   return (
@@ -8,6 +9,23 @@ export default function Home() {
         <title>ikeo | Buy Furniture Online</title>
         <meta name="description" content="Ikeo home page" />
       </Head>
+      <Navbar fluid={true} rounded={true}>
+        <Navbar.Brand href="https://flowbite.com/">
+          <img
+            src="https://www.ikea.com/sg/en/static/ikea-logo.f7d9229f806b59ec64cb.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="ikeo Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="/" active={true}>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/products">products</Navbar.Link>
+          <Navbar.Link href="/contact">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
       <h1>The ikeo Home Page</h1>
     </Fragment>
   )
