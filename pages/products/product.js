7eda548d1a56b696e36d5ed92281818c84fd44e3
charleshugochg <minheinkhant@hotmail.com>
2022-11-10 14:24:27 +0630

    mockup with some static data

--- pages/products/product.js
+++ pages/products/product.js
@@ -1,3 +1,27 @@
-export default function Product () {
-  return <h1>Product Page</h1>
+import { Fragment } from "react"
+
+export default function Product ({ name }) {
+  return (
+    <Fragment>
+      <h1>Product page</h1>
+      <p>{ name }</p>
+    </Fragment>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      id: 'id',
+      name: 'product name',
+      description: 'some description',
+      images: [
+        "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1012432_pe829085_s5.avif",
+        "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1017104_pe830725_s5.avif",
+        "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1079006_pe857430_s5.avif",
+        "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1079017_pe857434_s5.avif"
+      ],
+      summary: ""
+    }
+  }
 }
