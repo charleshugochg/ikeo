fab9554af484a54c0069787609cc67d90042faac
charleshugochg <minheinkhant@hotmail.com>
2022-11-10 15:07:26 +0630

    load images into the carousel

--- pages/products/product.js
+++ pages/products/product.js
@@ -1,10 +1,27 @@
 import { Fragment } from "react"
+import Image from "next/image"
 
-export default function Product ({ name }) {
+import { Carousel } from "flowbite-react"
+
+export default function Product ({ name, images }) {
   return (
     <Fragment>
       <h1>Product page</h1>
       <p>{ name }</p>
+      <div className="h-96 w-96 select-none">
+        <Carousel>
+          {images.map(image_url => (
+            <div key={image_url} className="w-full h-full relative">
+              <Image
+                src={image_url}
+                alt={name}
+                objectFit="cover"
+                fill
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </Fragment>
   )
 }
