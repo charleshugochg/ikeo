507ff2eeaf23acdabd05addae899bee14d222cff
charleshugochg <minheinkhant@hotmail.com>
2022-11-11 21:50:24 +0630

    highlight selected slide on thumbs

--- components/EmblaCarousel.js
+++ components/EmblaCarousel.js
@@ -1,9 +1,10 @@
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import Image from "next/image"
 
 import useEmblaCarousel from "embla-carousel-react"
 
 export default function EmblaCarousel({ name, images }) {
+  const [selectedIndex, setSelectedIndex] = useState(0)
   const [mainEmblaRef, mainEmbla] = useEmblaCarousel()
   const [thumbEmblaRef, thumbEmbla] = useEmblaCarousel({
     containScroll: "keepSnaps",
@@ -35,7 +36,7 @@ export default function EmblaCarousel({ name, images }) {
         <div className="flex select-none">
           {images.map((image_url, index) => (
             <div className="p-0.5">
-              <button key={index} className="h-20 w-20 relative shrink-0" onClick={() => onThumbClick(index)}>
+              <button key={index} className={`h-20 w-20 relative shrink-0 ${selectedIndex == index && 'ring-2 ring-cyan-600'}`} onClick={() => onThumbClick(index)}>
                 <Image
                   src={image_url}
                   alt={name}
