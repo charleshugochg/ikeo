b4bca5bed4054cffad0ee1c297d8f405ce392497
charleshugochg <minheinkhant@hotmail.com>
2022-11-11 21:56:49 +0630

    experiment with more images

--- pages/products/product.js
+++ pages/products/product.js
@@ -22,7 +22,12 @@ export async function getStaticProps() {
         "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1012432_pe829085_s5.avif",
         "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1017104_pe830725_s5.avif",
         "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1079006_pe857430_s5.avif",
-        "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1079017_pe857434_s5.avif"
+        "/images/idanaes-upholstered-ottoman-bed-gunnared-dark-grey__1079017_pe857434_s5.avif",
+        "/images/knoxhult-base-cabinet-with-doors-and-drawer-white__0628064_ph136514_s5.avif",
+        "/images/knoxhult-base-cabinet-with-doors-and-drawer-white__0630745_pe694873_s5.avif",
+        "/images/knoxhult-base-cabinet-with-doors-and-drawer-white__0871266_pe615157_s5.avif",
+        "/images/knoxhult-base-cabinet-with-doors-and-drawer-white__0871705_pe615165_s5.avif",
+        "/images/knoxhult-base-cabinet-with-doors-and-drawer-white__0871917_pe617191_s5.avif"
       ],
       summary: ""
     }
